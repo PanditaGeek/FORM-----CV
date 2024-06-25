f1d61138b2de7f86bbf755c957acd94cac323fdb
Jos <118079175+PanditaGeek@users.noreply.github.com>
2024-06-25 17:56:20 -0500

    Add files via upload

---
+++ FORM CV/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Formulario_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCreateCvButtonClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CurriculumPage(nombre.Text, apellido.Text, estudios.Text, experiencia.Text, area.Text, telefono.Text, correo.Text));
+        }
+    }
+}
---
+++ FORM CV/curriculum.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formulario_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CurriculumPage : ContentPage
+    {
+        public CurriculumPage(string nombre, string apellido, string estudios, string experiencia, string area, string telefono, string correo)
+        {
+            InitializeComponent();
+            nombreLabel.Text += nombre;
+            apellidoLabel.Text += apellido;
+            estudiosLabel.Text += estudios;
+            experienciaLabel.Text += experiencia + " años de experiencia";
+            areaLabel.Text += area;
+            telefonoLabel.Text += telefono;
+            correoLabel.Text += correo;
+        }
+    }
+}
